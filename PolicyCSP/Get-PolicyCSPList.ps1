de2d2b038b45edfd3e1c70e88d3fa231669f9d16
nicolonsky <32899754+nicolonsky@users.noreply.github.com>
2018-04-16 22:48:43 +0200

    optimized filtering

--- PolicyCSP/Get-PolicyCSPList.ps1
+++ PolicyCSP/Get-PolicyCSPList.ps1
@@ -1,10 +1,10 @@
-$policyCSPEntries=Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default" -Recurse | Where-Object {$_.PSIsContainer} | Select Name, PSChildName
+$PolicyCSPHive= "HKLM:\SOFTWARE\Microsoft\PolicyManager\default"
 
+$PolicyCSPHivePSPath= Get-Item $PolicyCSPHive | Select -ExpandProperty PSPath
 
-
+$policyCSPEntries=Get-ChildItem -Path $PolicyCSPHive -Recurse | Where {$_.PSParentPath -ne $PolicyCSPHivePSPath} | Select Name, PSChildName
+     
 $windosBuild= Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild
-
-
 
 $policyCSPEntries| Export-Csv -Path $(Join-Path -Path $PSScriptRoot -ChildPath "CSPPolicyList_W10-$($windosBuild.CurrentBuild).csv") -Delimiter "," -NoTypeInformation -Force
 
