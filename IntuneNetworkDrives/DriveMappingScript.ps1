4ac9ea2b693cbb4f37fae96c5a7d502a9a0fe441
nicolonsky <nicola@nicolonsky.ch>
2019-01-10 14:57:13 +0100

    New Blogpost

---
+++ IntuneNetworkDrives/DriveMappingScript.ps1
@@ -0,0 +1,62 @@
+﻿Start-Transcript -Path $(Join-Path $env:temp "DriveMapping.log")
+
+$dnsDomainName= "tech.nicolonsky.ch"
+$driveMappingConfig=@()
+
+$driveMappingConfig+= [PSCUSTOMOBJECT]@{
+    DriveLetter = "P"
+    UNCPath= "\\vfs01.tech.nicolonsky.ch\pool"
+    Description="Pool"
+}
+
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "T"
+    UNCPath= "\\vfs01.tech.nicolonsky.ch\software"
+    Description="Software"
+}
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "V"
+    UNCPath= "\\vfs01.tech.nicolonsky.ch\archiv"
+    Description="Archiv"
+}
+
+$connected=$false
+$retries=0
+$maxRetries=3
+
+Write-Output "Starting script..."
+do {
+    
+    if (Resolve-DnsName $dnsDomainName -ErrorAction SilentlyContinue){
+    
+        $connected=$true
+
+    } else{
+ 
+        $retries++
+        
+        Write-Warning "Cannot resolve: $dnsDomainName, assuming no connection to fileserver"
+ 
+        Start-Sleep -Seconds 3
+ 
+        if ($retries -eq $maxRetries){
+            
+            Throw "Exceeded maximum numbers of retries ($maxRetries) to resolve dns name ($dnsDomainName)"
+        }
+    }
+ 
+}while( -not ($Connected))
+
+#Map drives
+    $driveMappingConfig.GetEnumerator() | ForEach-Object {
+
+        Write-Output "Mapping network drive $($PSItem.UNCPath)"
+
+        New-PSDrive -PSProvider FileSystem -Name $PSItem.DriveLetter -Root $PSItem.UNCPath -Description $PSItem.Description -Persist -Scope global
+
+        (New-Object -ComObject Shell.Application).NameSpace("$($PSItem.DriveLEtter):").Self.Name=$PSItem.Description
+}
+
+Stop-Transcript
---
+++ IntuneNetworkDrives/InvokeDriveMapping.ps1
@@ -0,0 +1,14 @@
+﻿$azDriveMAppingScriptUrl= "https://ntintune.blob.core.windows.net/intune-scripts/DriveMappingScript.ps1"
+
+$regKeyLocation="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+
+$psCommand= "PowerShell.exe -ExecutionPolicy Bypass -windowstyle hidden -command $([char]34)& {(Invoke-RestMethod '$azDriveMAppingScriptUrl').Replace('ï','').Replace('»','').Replace('¿','') | Invoke-Expression}$([char]34)"
+
+if (-not(Test-Path -Path $regKeyLocation)){
+
+    New-ItemProperty -Path $regKeyLocation -Force
+}
+
+Set-ItemProperty -Path $regKeyLocation -Name "PowerShellDriveMapping" -Value $psCommand -Force
+
+Invoke-Expression $psCommand
