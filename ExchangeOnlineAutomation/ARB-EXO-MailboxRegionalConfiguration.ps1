c65859faa580a7ccf100f3a7336fcec7c674a9d1
Nicola Suter <32899754+nicolonsky@users.noreply.github.com>
2019-10-19 00:28:09 +0200

    Update ARB-EXO-MailboxRegionalConfiguration.ps1

--- ExchangeOnlineAutomation/ARB-EXO-MailboxRegionalConfiguration.ps1
+++ ExchangeOnlineAutomation/ARB-EXO-MailboxRegionalConfiguration.ps1
@@ -4,7 +4,7 @@ Connect-ExchangeOnlineShell -Credential $UserCredential
 
 Get-Mailbox -ResultSize unlimited -RecipientTypeDetails UserMailbox | Foreach-Object {
 
-    Set-MailboxRegionalConfiguration -Identity $PsItem.alias -Language "de-CH" -TimeZone "W. Europe Standard Time" -DateFormat "dd.MM.yyyy" -LocalizeDefaultFolderName
+    Set-MailboxRegionalConfiguration -Identity $PsItem.alias -Language "de-CH" -TimeZone "W. Europe Standard Time" -DateFormat "dd.MM.yyyy"
 }
 
 Disconnect-ExchangeOnlineShell
