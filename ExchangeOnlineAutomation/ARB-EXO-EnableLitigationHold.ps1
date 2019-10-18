0ad664e5d1f194a544b4d0bedbc52f0c039d394f
Nicola Suter <32899754+nicolonsky@users.noreply.github.com>
2019-10-19 00:28:44 +0200

    Update ARB-EXO-EnableLitigationHold.ps1

--- ExchangeOnlineAutomation/ARB-EXO-EnableLitigationHold.ps1
+++ ExchangeOnlineAutomation/ARB-EXO-EnableLitigationHold.ps1
@@ -5,7 +5,7 @@ Connect-ExchangeOnlineShell -Credential $UserCredential
 
 Get-Mailbox -ResultSize unlimited -RecipientTypeDetails UserMailbox | Foreach-Object {
 
-   Set-Mailbox mailbox@yourtenant.com -LitigationHoldEnabled $true -LitigationHoldDuration 3650
+   Set-Mailbox -Identity $PsItem.alias -LitigationHoldEnabled $true -LitigationHoldDuration 3650
 }
 
 Disconnect-ExchangeOnlineShell
