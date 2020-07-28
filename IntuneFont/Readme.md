5753e0ab4aa34bcb2abe01fd76a739489e054a5f
Nicola Suter <nicola@nicolonsky.ch>
2020-07-28 11:07:35 +0200

    Update font deployment scripts

--- IntuneFont/Readme.md
+++ IntuneFont/Readme.md
@@ -1,3 +1,5 @@
 # Deploy fonts to Intune managed Windows 10 devices
 
 [Documentation is available on my blog](https://tech.nicolonsky.ch/deploy-fonts-with-intune/)
+
+Place your *.ttf and *.otf files in the `fonts` folder.
--- IntuneFont/installfonts.ps1
+++ IntuneFont/installfonts.ps1
@@ -1,32 +1,54 @@
-$fontFolderName = "fonts"
+[CmdletBinding()]
+param ()
 
-$supportedFonts = @{
-    ".otf" = "OpenType"
-    ".ttf" = "TrueType"
+# Helper function
+function Install-Font {
+    [CmdletBinding()]
+    param (
+        # Path which points to font file (*.otf / *.ttf)
+        [Parameter(Mandatory)]
+        [ValidateScript( { Test-Path $_ })]
+        [string]
+        $FontPath
+    )
+    
+    begin {
+        $supportedFonts = @{
+            ".otf" = "OpenType"
+            ".ttf" = "TrueType"
+        }
+    }
+    
+    process {
+        try {
+            $font = Get-Item -Path $FontPath
+            $fontType = $font.Extension
+    
+            if ($supportedFonts.Keys -contains $fontType) {
+                
+                Write-Output "'$($font.Name)' is $($supportedFonts[$fontType]) font"
+                
+                # Copy and reference font
+                $fontRegistryLocation = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+                $null = Copy-Item -Path $font.FullName -Destination "C:\Windows\Fonts\" -Force -EA Stop
+                $null = New-ItemProperty -Name "$($font.BaseName) ($($supportedFonts[$fontType]))" -Path $fontRegistryLocation -PropertyType string -Value $font.Name -Force -EA Stop
+    
+                Write-Output "Installed font '$($font.Name)'"
+            }
+            else {
+                Write-Warning "Unrecognized font type '$($fontType)' $($font.Name)"
+            }
+        }
+        catch {
+            Write-Error $_
+        }
+    }
 }
 
+# Main script
+$fontFolderName = "fonts"
 $potentialFonts = Get-ChildItem -Path $(Join-Path $PSScriptRoot $fontFolderName)
 
-foreach ($font in $potentialFonts){
-    
-    try{
-
-        $fontType = $font.Extension
-
-        if ($supportedFonts.Keys -contains $fontType){
-            
-            Write-Output "$($font.Name) is $($supportedFonts[$fontType]) font"
-            
-            # Copy and reference font
-            $null = Copy-Item -Path $font.FullName -Destination "C:\Windows\Fonts\" -Force -EA Stop
-            $null = New-ItemProperty -Name "$($font.BaseName) ($($supportedFonts[$fontType]))" -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $font.Name -Force -EA Stop
-
-            Write-Output "Installed font $($font.Name)"
-        }else {
-            Write-Warning "Unrecognized font type '$($fontType)' $($font.Name)"
-        }
-
-    }catch{
-        Write-Error $_
-    }
+foreach ($font in $potentialFonts) {
+    Install-Font -FontPath $font.FullName
 }
